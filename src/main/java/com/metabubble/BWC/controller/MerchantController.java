8d7bbfeaa584bb67dc0f58f86e11e0daf6a255d5
leitianyu999 <1025596635@qq.com>
2022-07-20 23:40:32 +0800

    leitianyu:add MerchantController(001)

---
+++ src/main/java/com/metabubble/BWC/controller/MerchantController.java
@@ -0,0 +1,15 @@
+package com.metabubble.BWC.controller;
+
+
+import com.metabubble.BWC.mapper.MerchantMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/merchant")
+public class MerchantController {
+    @Autowired
+    private MerchantMapper merchantMapper;
+
+}
