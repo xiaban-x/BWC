233af7541f66053a35faa2ef5f84d70f2c349c9b
leitianyu999 <1025596635@qq.com>
2022-07-29 00:05:18 +0800

    leitianyu:update TaskService and TaskServiceImpl(001)

--- src/main/java/com/metabubble/BWC/service/TaskService.java
+++ src/main/java/com/metabubble/BWC/service/TaskService.java
@@ -4,4 +4,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.metabubble.BWC.entity.Task;
 
 public interface TaskService extends IService<Task> {
+
+    //查询任务是否生效
+    public Boolean checkTaskStatus(Long id);
+
 }
--- src/main/java/com/metabubble/BWC/service/impl/TaskServiceImpl.java
+++ src/main/java/com/metabubble/BWC/service/impl/TaskServiceImpl.java
@@ -9,4 +9,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskServiceImpl extends ServiceImpl<TaskMapper, Task>
         implements TaskService {
+
+    /**
+     * 查询任务是否有效
+     * @param id
+     * @return
+     * @author leitianyu999
+     */
+    @Override
+    public Boolean checkTaskStatus(Long id) {
+        Task task = this.getById(id);
+        if (task.getStatus()==1){
+            return true;
+        }
+        if (task.getStatus()==0){
+            return false;
+        }
+        return null;
+    }
 }
