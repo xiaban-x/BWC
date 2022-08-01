4717ad01b6ced296c700471fb670c412605281e4
leitianyu999 <1025596635@qq.com>
2022-08-01 23:03:23 +0800

    leitianyu:update UserServiceImpl(003)

--- src/main/java/com/metabubble/BWC/service/impl/TeamServiceImpl.java
+++ src/main/java/com/metabubble/BWC/service/impl/TeamServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.metabubble.BWC.entity.Admin;
 import com.metabubble.BWC.entity.Orders;
 import com.metabubble.BWC.entity.Team;
+import com.metabubble.BWC.entity.User;
 import com.metabubble.BWC.mapper.AdminMapper;
 import com.metabubble.BWC.mapper.TeamMapper;
 import com.metabubble.BWC.service.AdminService;
@@ -64,5 +65,33 @@ public class TeamServiceImpl extends ServiceImpl<TeamMapper, Team>
             team.setTotalWithdrawnAmount(team.getTotalWithdrawnAmount().add(bigDecimalForSecondWithNtoVip));
         }
         this.updateById(team);
+    }
+
+    /**
+     * 创建团队表
+     * @param id
+     */
+    @Override
+    public void save(Long id) {
+        Team team = new Team();
+        team.setId(id);
+        this.save(team);
+    }
+
+    /**
+     * 团队添加上一级和上二级
+     * @param user
+     */
+    @Override
+    public void addTeamTop(User user,User topUser) {
+        Team team = this.getById(user.getId());
+        Team teamTop = this.getById(topUser.getId());
+
+        team.setUpUser01Id(topUser.getId());
+        if (teamTop.getUpUser01Id()!=null){
+            team.setUpUser02Id(teamTop.getUpUser01Id());
+        }
+        this.updateById(team);
+
     }
 }
