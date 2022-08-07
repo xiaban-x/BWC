5c110666ea0c69f64e6c3e4adf642883565eafdc
xiaojie <1759747247@qq.com>
2022-08-07 14:12:20 +0800

    晴天小杰:日志功能更新

--- src/main/java/com/metabubble/BWC/service/LogsService.java
+++ src/main/java/com/metabubble/BWC/service/LogsService.java
@@ -11,4 +11,16 @@ public interface LogsService extends IService<Logs> {
     //存储管理员操作日志  (标题， 内容)
     public void saveLog(String name, String content);
 
+    /* 示例
+
+    @Autowired
+    private LogsService logsService;
+
+    //在对应的方法下面
+    public R Test(){
+        logsService.saveLog("标题","内容");
+    }
+
+     */
+
 }
