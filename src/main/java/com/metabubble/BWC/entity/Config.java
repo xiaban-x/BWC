613ec87c74f67092e8684c9bbc8a81b6d91d39d7
xiaojie <1759747247@qq.com>
2022-08-30 21:19:58 +0800

    晴天小杰：配置类更改

--- src/main/java/com/metabubble/BWC/entity/Config.java
+++ src/main/java/com/metabubble/BWC/entity/Config.java
@@ -21,7 +21,10 @@ public class Config implements Serializable {
     //配置内容
     private String content;
 
-    //配置类型，0为系统配置(默认)，1为用户端首页轮播图，2为公告信息，6为微信客服，7为QQ客服，8为客服邮箱，9为客服电话
+    //配置详细内容
+    private String contents;
+
+    //配置类型，0为系统配置(默认)，1为用户端首页轮播图，2为公告信息，3为隐私政策，4为用户协议，5为关于我们，6为微信客服，7为QQ客服，8为客服邮箱，9为客服电话
     private Integer type;
 
     //是否启用,0为禁用，1为启用(默认)
