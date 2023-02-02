4f49c60412eb90e8bb4df459a5e2632d2269a638
cclucky <2226114701@qq.com>
2023-02-02 23:42:59 +0800

    modified:   .idea/workspace.xml 	modified:   src/main/java/com/metabubble/BWC/config/HttpsConfiguration.java

--- src/main/java/com/metabubble/BWC/config/HttpsConfiguration.java
+++ src/main/java/com/metabubble/BWC/config/HttpsConfiguration.java
@@ -28,7 +28,9 @@ public class HttpsConfiguration {
                 securityConstraint.setUserConstraint("CONFIDENTIAL");
                 SecurityCollection collection = new SecurityCollection();
                 collection.addPattern("/*");
+
                 collection.addMethod(DEFAULT_PROTOCOL);
+                
                 securityConstraint.addCollection(collection);
                 context.addConstraint(securityConstraint);
             }
