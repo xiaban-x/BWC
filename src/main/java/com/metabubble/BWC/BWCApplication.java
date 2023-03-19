5136e17e23214dba81daff1a0ce16dda7b0129e5
xiaojie <1759747247@qq.com>
2023-03-19 15:18:14 +0800

    晴天小杰：解决数据库懒加载问题

--- src/main/java/com/metabubble/BWC/BWCApplication.java
+++ src/main/java/com/metabubble/BWC/BWCApplication.java
@@ -1,14 +1,22 @@
 package com.metabubble.BWC;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.metabubble.BWC.common.ManageSession;
+import com.metabubble.BWC.entity.Config;
+import com.metabubble.BWC.service.ConfigService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.TimeZone;
 
 @Slf4j//开启日志
@@ -16,6 +24,7 @@ import java.util.TimeZone;
 @ServletComponentScan//对Servlet组件扫描
 @EnableTransactionManagement //开启管理事务（多张表处理）
 @EnableCaching //开启缓存注解
+@EnableScheduling //开启定时任务
 public class BWCApplication {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
@@ -31,4 +40,17 @@ public class BWCApplication {
     static {
         System.setProperty("druid.mysql.usePingMethod","false");
     }
+
+    @Autowired
+    private ConfigService configService;
+    //定时调用客服接口（解决数据库懒加载问题）
+    @Scheduled(cron="0 0/5 * * * ?")   //每5分钟执行一次
+    public void execute(){
+        LambdaQueryWrapper<Config> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Config::getType,9);
+        List<Config> list = configService.list(queryWrapper);
+        System.out.println(list);
+    }
+
+
 }
